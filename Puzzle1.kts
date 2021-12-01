589925235de228ae078c08960279d642d91d795a
ramsilva <ricardo.silva@sky.uk>
2021-12-01 13:12:52 +0000

    puzzle1.1

--- Puzzle1.kts
+++ Puzzle1.kts
@@ -1,8 +1,12 @@
 import java.io.File
 
-val puzzle1 = File("input/puzzle1/input.txt").readLines().zipWithNext().count { it.first.toInt() < it.second.toInt() }
+val lines = File("input/puzzle1/input.txt").readLines()
+
+val puzzle1 = lines.zipWithNext().count { it.first.toInt() < it.second.toInt() }
 println(puzzle1)
 
-fun solvePuzzle1dot1(): Int {
-    return 0
-}
+val puzzle1dot1 = lines.mapIndexed { index, s ->
+    if(index + 2 >= lines.size) 0
+    else s.toInt() + lines[index+1].toInt() + lines[index+2].toInt()
+}.zipWithNext().count { it.first.toInt() < it.second.toInt() }
+println(puzzle1dot1)
