9de88247a2bf4919343bccaf56455ba768e730ee
ramsilva <ricardo.silva@sky.uk>
2021-12-09 11:50:21 +0000

    Add comments and make code more legible

--- src/main/kotlin/puzzles/Puzzle9.kt
+++ src/main/kotlin/puzzles/Puzzle9.kt
@@ -4,8 +4,20 @@ import java.io.File
 
 private val lines = File("input/puzzle9/input.txt").readLines().toMutableList()
 
-fun MutableList<MutableList<Int>>.getValue(x: Int, y: Int) =
+fun List<List<Int>>.getValue(x: Int, y: Int) =
     ((this.getOrNull(y)) ?: emptyList()).getOrNull(x) ?: Int.MAX_VALUE
+
+fun Pair<Int, Int>.adjacentPositions() = listOf(
+    Pair(this.first - 1, this.second),
+    Pair(this.first + 1, this.second),
+    Pair(this.first, this.second - 1),
+    Pair(this.first, this.second + 1)
+)
+
+fun isLowestPoint(map: List<List<Int>>, position: Pair<Int, Int>) =
+    position.adjacentPositions().all { adjacentPosition ->
+        map.getValue(position.first, position.second) < map.getValue(adjacentPosition.first, adjacentPosition.second)
+    }
 
 fun puzzle9(): Int {
     val map = mutableListOf<MutableList<Int>>()
@@ -20,15 +32,11 @@ fun puzzle9(): Int {
         map.add(row)
     }
 
-    return map.foldIndexed(mutableListOf<Int>()) { iy, acc, row ->
-        row.forEachIndexed { ix, number ->
-            val ys = intArrayOf(iy - 1, iy, iy + 1)
-            val xs = intArrayOf(ix - 1, ix, ix + 1)
-            if (ys.all { y ->
-                    xs.all { x ->
-                        number < map.getValue(x, y) || (y == iy && x == ix)
-                    }
-                }) acc.add(number)
+    return map.foldIndexed(mutableListOf<Int>()) { y, acc, row ->
+        row.forEachIndexed { x, number ->
+            if (isLowestPoint(map, Pair(x, y))) {
+                acc.add(number)
+            }
         }
         acc
     }.sumOf { it + 1 }
@@ -38,51 +46,28 @@ fun puzzle9(): Int {
 fun puzzle9dot1(): Int {
     val map = mutableListOf<MutableList<Int>>()
 
-    fun isLowestPoint(pos: Pair<Int, Int>): Boolean {
-        val (posX, posY) = pos
-        val ys = intArrayOf(posY - 1, posY, posY + 1)
-        val xs = intArrayOf(posX - 1, posX, posX + 1)
-
-        return (ys.all { y ->
-            xs.all { x ->
-                map.getValue(posX, posY) < map.getValue(x, y) || (y == posY && x == posX)
-            }
-        })
-    }
-
     fun growBasin(
-        pos: List<Pair<Int, Int>>,
+        currentBasin: List<Pair<Int, Int>>,
         checkedPos: List<Pair<Int, Int>>
     ): List<Pair<Int, Int>> {
 
         // fUnCtIoNaL PrOgRAmMiNg
-        val newBasin = pos.toMutableList()
-        val checkPosCopy = checkedPos.toMutableList()
+        val newBasin = currentBasin.toMutableList()
+        val checkedPosCopy = checkedPos.toMutableList()
 
-        pos.forEach { p ->
-            val (posX, posY) = p
-
-            // get the 4 adjacent coordinates
-            val posToCheck = listOf(
-                Pair(posX - 1, posY),
-                Pair(posX + 1, posY),
-                Pair(posX, posY - 1),
-                Pair(posX, posY + 1)
-            )
-
-            posToCheck.forEach {
-                val (x, y) = it
-
-                val newPos = Pair(x, y)
-                if (map.getValue(x, y) < 9 && !checkPosCopy.contains(newPos)) {
-                    newBasin.add(newPos)
-                    checkPosCopy.add(newPos)
+        currentBasin.forEach { basinPosition ->
+            basinPosition.adjacentPositions().forEach { adjacentPosition ->
+                if (map.getValue(adjacentPosition.first, adjacentPosition.second) < 9
+                    && !checkedPosCopy.contains(adjacentPosition)
+                ) {
+                    newBasin.add(adjacentPosition)
+                    checkedPosCopy.add(adjacentPosition)
                 }
             }
         }
 
-        return if (newBasin.size == pos.size) pos // if there were no new positions added, the basin is complete
-        else growBasin(newBasin, checkPosCopy) // recursion baby
+        return if (newBasin.size == currentBasin.size) currentBasin // if there were no new positions added, the basin is complete
+        else growBasin(newBasin, checkedPosCopy) // recursion baby
     }
 
     // create the map
@@ -100,7 +85,7 @@ fun puzzle9dot1(): Int {
 
     map.forEachIndexed { y, row ->
         row.forEachIndexed { x, _ ->
-            if (isLowestPoint(Pair(x, y))) {
+            if (isLowestPoint(map, Pair(x, y))) {
                 val basin = growBasin(mutableListOf(Pair(x, y)), mutableListOf(Pair(x, y)))
 
                 basins.add(basin)
